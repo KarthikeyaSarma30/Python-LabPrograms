552e24bc54f577186fafe1609c55117b93a09d1d
KarthikeyaSarma30 <77091365+KarthikeyaSarma30@users.noreply.github.com>
2021-09-15 17:54:09 +0530

    Add files via upload

---
+++ 2_1.py
@@ -0,0 +1,7 @@
+#distance between two points
+x1=int(input("enter x1 value"))
+y1=int(input("enter y1 value"))
+x2=int(input("enter x2 value"))
+y2=int(input("enter y2 value"))
+distance=((x2-x1)**2+(y2-y1)**2)**0.5
+print("Distance between two points:",distance)
